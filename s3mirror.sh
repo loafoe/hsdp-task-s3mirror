48cc17b09169756e2a3e7af5243f0152b6f4ff7f
AndrewPentsak <partner.andriy.pentsak@philips.com>
2022-07-19 12:55:07 +0300

    Added overwrite flag for s3 mirror

--- s3mirror.sh
+++ s3mirror.sh
@@ -3,4 +3,4 @@
 mc alias set source https://${source_endpoint} ${source_access_key} ${source_secret_key}
 mc alias set dest https://${dest_endpoint} ${dest_access_key} ${dest_secret_key}
 
-mc mirror source/${source_bucket}${source_prefix} dest/${dest_bucket}${dest_prefix}
+mc mirror --overwrite source/${source_bucket}${source_prefix} dest/${dest_bucket}${dest_prefix}
